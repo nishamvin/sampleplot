3b074d9d9a4e68b8628fa6d7dc3c5e8158642435
nishamvin <75119322+nishamvin@users.noreply.github.com>
2021-12-05 23:28:23 +0530

    Add files via upload

---
+++ sample1.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+import plotly.express as px
+
+
+#dict = {'Name': 'Bitan', 'Age': 14, 'Class': 'Tenth'}
+#print("dict['Name']: ", dict['Name'])
+#print("dict['Age']: ", dict['Age'])
+
+
+#df={'x':[1,2,3],'y':[1,2,3]}
+
+
+x=[1,2,3]
+y=[1,2,3]
+
+fig = px.scatter(x=x, y=y)
+fig.show()
+#1+2+3
+#print(sum(a))
+
+
+ 
+# data to be plotted
+
+ 
+# plotting
---
+++ sample2.py
@@ -0,0 +1,9 @@
+print("my name is Bitan")
+
+
+a=input("enter first number")
+b=input("enter second number")
+
+c=int(a)+int(b)
+
+print(c)
